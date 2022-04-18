b12747d56f5549843600f700d8e869b82237504a
iamprofessor1 <vikaspandey12062001@gmail.com>
2022-04-18 22:13:00 +0530

    Project Euler #1: Multiples of 3 and 5

---
+++ Project_Euler_1_Multiples_of_3_and_5.cpp
@@ -0,0 +1,82 @@
+//** 𝓙ⲁⲓ Ⲙⲁⲏⲁⲕⲁ𝓵 **//**// हर हर महादेव**//
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+using namespace std;
+#define ln "\n"
+#define Ln "\n"
+#define ll long long
+#define ull unsigned long long
+#define int long long
+#define double long double
+#define vll vector<ll>
+#define eb emplace_back
+#define pll pair<ll, ll>
+#define vpll vector<pll>
+#define fi first
+#define se second
+#define mod 1000000007
+#define mp make_pair
+#define sz(x) ((ll)(x).size())
+#define set_bits __builtin_popcountll
+#define all(x) (x).begin(), (x).end()
+#define fo(m) for (auto &x : m)
+#define yes cout << "YES" << Ln;
+#define no cout << "NO" << Ln;
+#define queue_max priority_queue<int>
+#define queue_min priority_queue<int, vi, greater<int>>
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+#define uniq(v) (v).erase(unique(all(v)), (v).end())
+#define precise(ans) cout << fixed << setprecision(15) << ans << ln
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+template <typename Arg1>
+void __f(const char *name, Arg1 &&arg1)
+{
+    cerr << name << " : " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&...args)
+{
+    const char *comma = strchr(names + 1, ',');
+    cerr.write(names, comma - names) << " : " << arg1 << " | ";
+    __f(comma + 1, args...);
+}
+
+void solve()
+{
+
+    ll n;
+    cin >> n;
+    ll ans3 = ((3 * ((n - 1) / 3) * ((n - 1) / 3 + 1))) / 2;
+    ll ans5 = ((5 * ((n - 1) / 5) * ((n - 1) / 5 + 1))) / 2;
+    ll ans15 = ((15 * ((n - 1) / 15) * ((n - 1) / 15 + 1))) / 2;
+
+    // trace(ans3, ans5,ans15);
+    // trace1(ans15);
+    cout << ans3 + ans5 - ans15 << Ln;
+}
+
+int32_t main()
+{
+    fast_cin();
+    ll T = 1;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+
+    //  ll t;
+    //  cin >> t;
+    //  for(int it=1;it<=t;it++) {
+    //	 cout << "Case #" << it << ": ";
+    //    solve();
+    //  }
+
+    return 0;
+}
+//** Har Har Mahadev **//
